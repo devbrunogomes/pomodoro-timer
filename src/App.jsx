8b2aaa8fd3d136e9ea47702fb7a6fd97b95de0d6
Bruno Gomes <bsgomes16@gmail.com>
2024-03-06 15:33:07 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { Timer } from './components/Timer/Timer';
+
+function App() {
+  return (
+    <>      
+      <Timer></Timer>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Timer/Timer.jsx
@@ -0,0 +1,9 @@
+export const Timer = () => {
+  return (
+    <div>
+      <h1>
+        Timer
+      </h1>
+    </div>
+  )
+}
